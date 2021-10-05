9c5374987cb02139aca4a5391f03a76f211d23f1
Belig Chimitov <belig.ch111@gmail.com>
2021-10-05 14:41:34 +0700

    third commit
    Added toPower function with default 'power' value equal 2

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 console.log("Hello, world!\n" +
     "This message is from console.log command in app.js")
 
-
-
-
-
-
 function sum (num1, num2){
     return num1 + num2;
 }
 
 const result1 = sum(3,4)
 console.log(`3 + 4 = ${result1}`)
+
+
+function toPower(num, power= 2){
+    return Math.pow(num, power)
+}
+
+const result2 = toPower(2)
+console.log(`2 to power of 2 = ${result2}`)
