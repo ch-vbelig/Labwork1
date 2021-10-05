7a6360670ad500abfbd9cb35afccd073b950d80d
Belig Chimitov <belig.ch111@gmail.com>
2021-10-05 14:58:26 +0700

    fifth commit
    Added multiply function

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ function subtract(num1, num2){
     return num1 - num2
 }
 
+function multiply(num1, num2){
+    return num1*num2
+}
+
 function toPower(num, power= 2){
     return Math.pow(num, power)
 }
@@ -19,5 +23,8 @@ console.log(`3 + 4 = ${resultOfSumming}`)
 const resultOfSubtraction = subtract(20, 13)
 console.log(`20 - 13 = ${resultOfSubtraction}`)
 
-const resultOfSuPowering = toPower(2)
-console.log(`2 to power of 2 = ${resultOfSuPowering}`)
+const resultOfPowering = toPower(2)
+console.log(`2 to power of 2 = ${resultOfPowering}`)
+
+const resultOfMultiplication = multiply(4, 6)
+console.log(`4 * 6 = ${resultOfMultiplication}`)
