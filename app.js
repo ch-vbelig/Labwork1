b677bfa76cadc138b9231311b94748b9217e867b
Belig Chimitov <belig.ch111@gmail.com>
2021-10-05 14:46:35 +0700

    fourth commit
    Added subtract function, tested on values 20 and 13

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-
-console.log("Hello, world!\n" +
-    "This message is from console.log command in app.js")
-
 function sum (num1, num2){
     return num1 + num2;
 }
 
-const result1 = sum(3,4)
-console.log(`3 + 4 = ${result1}`)
-
+function subtract(num1, num2){
+    return num1 - num2
+}
 
 function toPower(num, power= 2){
     return Math.pow(num, power)
 }
 
-const result2 = toPower(2)
-console.log(`2 to power of 2 = ${result2}`)
+console.log("Hello, world!\n" +
+    "This message is from console.log command in app.js")
+
+const resultOfSumming = sum(3,4)
+console.log(`3 + 4 = ${resultOfSumming}`)
+
+const resultOfSubtraction = subtract(20, 13)
+console.log(`20 - 13 = ${resultOfSubtraction}`)
+
+const resultOfSuPowering = toPower(2)
+console.log(`2 to power of 2 = ${resultOfSuPowering}`)
