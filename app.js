5f6fda3ae9240a09928abe842f6bd3e33f36ecdd
Belig Chimitov <belig.ch111@gmail.com>
2021-10-05 14:37:07 +0700

    second commit
    Added sum function and tested on values 3 and 4

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 
 console.log("Hello, world!\n" +
     "This message is from console.log command in app.js")
+
+
+
+
+
+
+function sum (num1, num2){
+    return num1 + num2;
+}
+
+const result1 = sum(3,4)
+console.log(`3 + 4 = ${result1}`)
